56f938b8d4d2d97ad589c46fd450fe1e35865f98
Guilherme <guilherme1984.araujo@gmail.com>
2024-02-26 21:57:19 -0300

    finished my firt app android with dio

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My firt app with Dio"
+include(":app")
